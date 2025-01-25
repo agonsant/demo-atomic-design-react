fc67b13dea30365422c1907b385a4923fb405e98
Alex González <alejandrogsa@gmail.com>
2025-01-25 13:35:21 -0300

    feat: add Page Title Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import "./ui-kit/atoms/icon/font-icons.css";
 import Subtitle from "./ui-kit/atoms/text/subtitle";
 import Icon from "./ui-kit/atoms/icon";
+import PageTitle from "./ui-kit/atoms/text/page-title";
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <PageTitle>Titulo de página</PageTitle>
       <Subtitle>Subtítulo por defecto</Subtitle>
       <Subtitle size="xs">Subtítulo pequeño</Subtitle>
       <Subtitle size="md">Subtítulo mediano</Subtitle>
