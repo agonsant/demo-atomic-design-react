931fd01f97f7df078fe72623d24ccb8e3d8fe8ff
Alex González <alejandrogsa@gmail.com>
2025-01-25 13:22:23 -0300

    chore: test component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import "./ui-kit/atoms/icon/font-icons.css";
+import Subtitle from "./ui-kit/atoms/text/subtitle";
+import Icon from "./ui-kit/atoms/icon";
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <span className="cb-icon-checkmark-circle"></span>
+      <Subtitle>Subtítulo por defecto</Subtitle>
+      <Subtitle size="xs">Subtítulo pequeño</Subtitle>
+      <Subtitle size="md">Subtítulo mediano</Subtitle>
+      <Subtitle size="lg">Subtítulo grande</Subtitle>
+
+      <Icon name="checkmark-circle" />
+      <Icon name="search" />
+
+      <Subtitle size="lg">
+        Subtítulo con Icono: <Icon name="search" />
+      </Subtitle>
     </>
   );
 }
