50583129ec0757bd37529f0ab4cd2104da51bb45
Alex González <alejandrogsa@gmail.com>
2025-01-29 15:35:53 -0300

    refactor: add primitive tokens dependents of branding token

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./ui-kit/tokens/primitives.css";
 import "./ui-kit/tokens/my-company-brand.css";
 import "./index.css";
 import App from "./App.jsx";
